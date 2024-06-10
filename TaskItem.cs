bda5456fba1d3d3d41052705fd753f1da5b50915
anshagarova <149403028+anshagarova@users.noreply.github.com>
2024-06-10 19:06:38 +0200

    UI update

--- TaskItem.cs
+++ TaskItem.cs
@@ -2,32 +2,48 @@ using System.ComponentModel;
 
 public class TaskItem : INotifyPropertyChanged
 {
-    private string description;
-    private bool isImportant;
-    public string Description 
-    { 
-        get => description; 
-        set { description = value; OnPropertyChanged(nameof(Description)); } 
-    }
-    
-    public bool IsImportant 
-    { 
-        get => isImportant; 
-        set { isImportant = value; OnPropertyChanged(nameof(IsImportant)); OnPropertyChanged(nameof(DisplayColor)); } 
-    }
-
-    public string DisplayColor => IsImportant ? "Red" : "Black";
+    private string _description;
+    private bool _isImportant;
 
     public event PropertyChangedEventHandler PropertyChanged;
+
+    public string Description
+    {
+        get => _description;
+        set
+        {
+            if (_description != value)
+            {
+                _description = value;
+                OnPropertyChanged(nameof(Description));
+            }
+        }
+    }
+
+    public bool IsImportant
+    {
+        get => _isImportant;
+        set
+        {
+            if (_isImportant != value)
+            {
+                _isImportant = value;
+                OnPropertyChanged(nameof(IsImportant));
+                OnPropertyChanged(nameof(DisplayColor));
+            }
+        }
+    }
 
     protected void OnPropertyChanged(string propertyName)
     {
         PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
     }
-    
+
+    public string DisplayColor => IsImportant ? "#fb8500" : "Black";
+
     public TaskItem(string description, bool isImportant)
     {
-        Description = description;
-        IsImportant = isImportant;
+        _description = description;
+        _isImportant = isImportant;
     }
 }
