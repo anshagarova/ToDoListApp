bf89ba728b8e95a836e3a86339c8a40458e86432
anshagarova <149403028+anshagarova@users.noreply.github.com>
2024-06-10 18:32:15 +0200

    UI update

--- MainWindow.axaml.cs
+++ MainWindow.axaml.cs
@@ -105,16 +105,4 @@ namespace ToDoListApp
             }
         }
     }
-
-    public class TaskItem
-    {
-        public string Description { get; set; }
-        public bool IsImportant { get; set; }
-
-        public TaskItem(string description, bool isImportant)
-        {
-            Description = description;
-            IsImportant = isImportant;
-        }
-    }
 }
--- TaskItem.cs
+++ TaskItem.cs
@@ -1,11 +1,43 @@
-   public class TaskItem
-    {
-        public string Description { get; set; }
-        public bool IsImportant { get; set; }
+using System.ComponentModel;
 
-        public TaskItem(string description, bool isImportant) 
+public class TaskItem : INotifyPropertyChanged
+{
+    private string description;
+    private bool isImportant;
+
+    public string Description
+    {
+        get => description;
+        set
         {
-            Description = description;
-            IsImportant = isImportant;
+            description = value;
+            OnPropertyChanged(nameof(Description));
         }
     }
+
+    public bool IsImportant
+    {
+        get => isImportant;
+        set
+        {
+            isImportant = value;
+            OnPropertyChanged(nameof(IsImportant));
+            OnPropertyChanged(nameof(DisplayColor));
+        }
+    }
+
+    public TaskItem(string description, bool isImportant)
+    {
+        Description = description;
+        IsImportant = isImportant;
+    }
+
+    public event PropertyChangedEventHandler PropertyChanged;
+
+    protected void OnPropertyChanged(string name)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+    }
+
+    public string DisplayColor => IsImportant ? "Red" : "Black";
+}
